c11ed7771252511b837cbea091db310bbc61a905
Wassim BEN MESSAOUD <wassim.benmessaoud@radiofrance.com>
2022-12-14 16:40:17 +0100

    first commit

--- GovernanceTest.py
+++ GovernanceTest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 #driver = webdriver.Firefox("/Users/wassimzenity/Downloads")
-driver = webdriver.Chrome("/Users/wassimzenity/Downloads")
+driver = webdriver.Chrome()
 
 driver.get("https://gov.dilitrust.com/")
 time.sleep(2)
