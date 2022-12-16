a7df240d92983efe5138df00e5a679226002152d
wassim.benmessaoud <wassim.benmessaoud@radiofrance.com>
2022-12-16 12:21:39 +0100

    test login ok

--- GovernanceTest.py
+++ GovernanceTest.py
@@ -3,23 +3,27 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome("/Users/wassimzenity/IdeaProjects/chromedriver")
-
+driver = webdriver.Chrome()
 driver.get("https://gov.dilitrust.com/")
 time.sleep(5)
 
 mailField = driver.find_element(By.ID, "login-name-input")
 mailField.send_keys("wassimbenmessaoud@gmail.com")
+time.sleep(5)
 
-loginButton = driver.find_element(By.ID, "login-button-button")
+loginButton = driver.find_element(By.ID, "login-button")
 loginButton.click()
+time.sleep(5)
 
 passwordField = driver.find_element(By.ID, "login-password-input")
 passwordField.send_keys("wassimbenmessaoud")
+time.sleep(5)
 
 loginButton.click()
+time.sleep(5)
 
-errorMessage = driver.find_element(By.ID, "login-form-error-0-message")
-assert (errorMessage.getAttribute("data-v-4d394ef5") == "Désolé, e-mail ou mot de passe non reconnu. ")
+errorMessage = driver.find_element(By.ID, "login-form-error-0-message").text
+
+assert (errorMessage == "Désolé, e-mail ou mot de passe non reconnu. Avez-vous oublié votre mot de passe ?")
 
 driver.close()
